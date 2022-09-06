69cc1a8238e68c2cd2fbec2de5502e63e164c5b7
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-09-06 06:13:42 -0700

    main

--- main.h
+++ main.h
@@ -1,42 +1,48 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> /*for varyadic functions*/
-#include <stdlib.h> /*for malloc and NULL*/
-#include <unistd.h> /*for write*/
-#include <limits.h> /* for test cases*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
 
 /**
- * struct special_case - struct for the special cases
- * @match: the special character to match after find a percentage
- * @function: the associated function to be called in each specialcase
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
  */
 
-typedef struct special_case
+typedef struct format
 {
-	char *match;
-	int (*function)(va_list);
-} spc_t;
+	char *id;
+	int (*f)();
+}convert_match;
 
-/*format and print data*/
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_unsigned(va_list args);
+int printf_oct(va_list val);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
 
-/*search for match and execute the function according to this*/
-int (*mod_character_s(const char *next, int dino))(va_list);
-
-/*prints a char */
-int print_char(va_list c);
-
-/*prints a string */
-int print_string(va_list s);
-
-/* convert a string to an integer and prints its content. */
-int print_number(va_list i);
-
-/*convert a string to an unsigned integer and prints its content */
-int print_unsigned(va_list u);
-
-/*prints a string in reverse */
-int print_reverse(va_list r);
-
-#endif /* MAIN_H */
+#endif
